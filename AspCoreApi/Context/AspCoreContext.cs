2bb0f64b9c3ed285f928572ba21831e43b5f48d3
MatheusMoraes <55507646+MatheusMoraes@users.noreply.github.com>
2021-10-24 22:08:29 -0300

    Initial + Configuração do Swagger

---
+++ AspCoreApi/Context/AspCoreContext.cs
@@ -0,0 +1,17 @@
+﻿using AspCoreApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspCoreApi.Context
+{
+    public class AspCoreContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AspCoreContext(DbContextOptions<AspCoreContext>options): base(options)
+        {}
+    }
+}
